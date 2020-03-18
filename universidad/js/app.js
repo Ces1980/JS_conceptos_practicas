92cfb956b0288813df53b0791ebb8e2bfa8d0cd3
CesarVE <thecesarvargas@gmail.com>
2020-03-18 14:45:37 -0600

    Eventos con el maouse

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,8 +1,28 @@
 /*Event Listener click al buscador*/
 
-document.querySelector('#submit-buscador').addEventListener('click',
-    function(e) {
-        /**  e.preventDefault(); evita que se realice una acción*/
-        e.preventDefault();
-        alert('Buscando Cursos');
-    });
+const encabezado = document.querySelector('#encabezado');
+const enlaces = document.querySelectorAll('.enlace');
+const boton = document.querySelector('#vaciar-carrito');
+
+//Click
+//boton.addEventListener('click', obtenerEvento);
+
+//Doble click
+//boton.addEventListener('dblclick', obtenerEvento);
+
+//Mouse enter
+boton.addEventListener('mouseenter', obtenerEvento);
+boton.addEventListener('mouseleave', obtenerEvento);
+boton.addEventListener('mouseover', obtenerEvento);
+boton.addEventListener('mouseout', obtenerEvento);
+boton.addEventListener('mousedown', obtenerEvento);
+boton.addEventListener('mouseup', obtenerEvento);
+boton.addEventListener('mousemove', obtenerEvento);
+
+function obtenerEvento(e) {
+    console.log(`Evento: ${e.type}`);
+}
+
+boton.addEventListener('dblclick', function(e) {
+    console.log('Doble click');
+});
