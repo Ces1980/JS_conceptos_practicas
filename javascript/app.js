d89633a9a6150c72e5c958862616a3696c5e131e
CesarVE <thecesarvargas@gmail.com>
2020-03-17 20:54:40 -0600

    For negativo y positivo en javascript

--- javascript/app.js
+++ javascript/app.js
@@ -1,42 +1,23 @@
-//Swith en JavaScript
-const metodoPago = 'tarjeta de debito';
-switch (metodoPago) {
-    case 'efectivo':
-        console.log('Usuario pago con efectivo');
-        break;
-    case 'tajeta de credito':
-        console.log('Usuario pago con tarjeta de credito');
-        break;
-    case 'tarjeta de debito':
-        console.log('Usuario pago con tarjeta de debito');
-        break;
-    case 'cheque':
-        console.log('Usuario pago con cheque');
-        break;
-}
+//For en JavaScript
 
-let numero = parseInt(prompt('Ingresa un número'));
-switch (numero) {
-    case 1:
-        console.log('Ingreso el número uno');
+for (let index = 0; index <= 20; index++) {
+
+    if (index === 10) {
+        console.log('Voy en el 10');
+        continue;
+    } else if (index == 15) {
         break;
-    case 2:
-        console.log('Ingreso el número dos');
-        break;
-    case 3:
-        console.log('Ingreso el número tres');
-        break;
-    case 4:
-        console.log('Ingreso el número cuatro');
-        break;
-    case 5:
-        console.log('Ingreso el número cinco');
-        break;
-    default:
-        if (numero > 3) {
-            console.log('Ingreso un número que no existe en la base de datos');
-        } else if (numero != Number) {
-            console.log('Ingreso un string');
-        }
-        break;
+
+    }
+    console.log(index);
+}
+console.log('Me sali en el 15');
+
+console.log('For negativo');
+
+for (let index = 20; index > 0; index--) {
+
+
+    console.log(index);
+
 }
