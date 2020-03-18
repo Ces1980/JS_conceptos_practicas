d0ea401f88cf88a8d9d53b0bd2c49d513f3113b4
CesarVE <thecesarvargas@gmail.com>
2020-03-17 20:45:24 -0600

    Switch en javaScript

--- javascript/app.js
+++ javascript/app.js
@@ -1,17 +1,42 @@
-function obtenerClientes() {
-    console.log('Descargando...');
-
-    setTimeout(function() {
-        console.log('Completo');
-    }, 5000);
+//Swith en JavaScript
+const metodoPago = 'tarjeta de debito';
+switch (metodoPago) {
+    case 'efectivo':
+        console.log('Usuario pago con efectivo');
+        break;
+    case 'tajeta de credito':
+        console.log('Usuario pago con tarjeta de credito');
+        break;
+    case 'tarjeta de debito':
+        console.log('Usuario pago con tarjeta de debito');
+        break;
+    case 'cheque':
+        console.log('Usuario pago con cheque');
+        break;
 }
-obtenerClientes();
 
-try {
-    algo();
-} catch (error) {
-    console.error(error);
-    console.warn(error);
-} finally {
-    console.log('Ejecute el código de una ');
+let numero = parseInt(prompt('Ingresa un número'));
+switch (numero) {
+    case 1:
+        console.log('Ingreso el número uno');
+        break;
+    case 2:
+        console.log('Ingreso el número dos');
+        break;
+    case 3:
+        console.log('Ingreso el número tres');
+        break;
+    case 4:
+        console.log('Ingreso el número cuatro');
+        break;
+    case 5:
+        console.log('Ingreso el número cinco');
+        break;
+    default:
+        if (numero > 3) {
+            console.log('Ingreso un número que no existe en la base de datos');
+        } else if (numero != Number) {
+            console.log('Ingreso un string');
+        }
+        break;
 }
