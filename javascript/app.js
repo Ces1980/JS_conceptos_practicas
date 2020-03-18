725a01d77d84f9d2cd2311234cca6369f8d53f8a
CesarVE <thecesarvargas@gmail.com>
2020-03-17 20:21:44 -0600

    Declaración try catch

--- javascript/app.js
+++ javascript/app.js
@@ -1,30 +1,17 @@
-//funciones IIFE --> Expresiones de función ejecutadas inmediatamente
+function obtenerClientes() {
+    console.log('Descargando...');
 
-
-(function() {
-    console.log('Creando un IIFE');
-})();
-
-
-(function(tecnologia) {
-    console.log(`Aprendiendo ${tecnologia}`);
-})('JavaScript');
-
-//Métodos de propiedad --> Cuando una función se pone dentro de un objeto
-
-const musica = {
-    reproducir: function() {
-        console.log('Reproduciendo música');
-    },
-    pausar: function() {
-        console.log('Pause a la música');
-    }
+    setTimeout(function() {
+        console.log('Completo');
+    }, 5000);
 }
+obtenerClientes();
 
-//Los métodos también pueden guardarse /crearse fuera del objeto
-musica.borrar = function(id) {
-    console.log(`Borrando canción con el ID ${id}`);
+try {
+    algo();
+} catch (error) {
+    console.error(error);
+    console.warn(error);
+} finally {
+    console.log('Ejecute el código de una ');
 }
-musica.reproducir();
-musica.pausar();
-musica.borrar(5);
