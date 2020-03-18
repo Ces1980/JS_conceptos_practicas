9db355a2f7672627c701ff525dc53a0848aa2e83
CesarVE <thecesarvargas@gmail.com>
2020-03-17 18:23:54 -0600

    Objetos => atributos, objetos y funciones

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,50 @@
-//Modificando valores de un arreglo cunado se declara un arreglo como constante
+///Crear objetos en JavaScript
 
-const numeros = [1, 3, 5, 7];
-console.log('Arreglo sin reasignar valores a sus elementos ' + numeros);
-console.log('Reasignando valor al elemento 2(vale 5) por el valor20 em el arreglo ');
-numeros[2] = 20;
-console.log(numeros);
+const persona = {
+    nombre: 'César',
+    apellido: 'Vargas',
+    profesion: 'Estudiante',
+    correo: 'cesar@vargas.com',
+    edad: '39',
+    musica: ['Rap', 'Pop', 'Regional mexicano'],
+    /*Arreglar objeto dentro de un objeto*/
+    hogar: {
+        ciudad: 'Ecatepec',
+        colonia: 'Ejidos de San Cristobal',
+        estado: 'estado de México',
+        pais: 'México'
+    },
+    /*Función declarada dentro de un objeto*/
+    nacimiento: function() {
+        let fechaDeNacimiento = new Date('1980-05-25');
+        let hoy = new Date();
+        return parseInt((hoy - fechaDeNacimiento) / (1000 * 60 * 60 * 24 * 365));
+    }
 
-/*El arreglo declarado no se puede reasignar pero si se pueden modificar sus elementos individualmente*/
+}
+
+console.log(persona);
+
+console.log('Mandando llarmar un elemento del objeto, para el ejemplo el apellido : ' + persona.apellido);
+
+console.log('Imprimir el arreglo declarado dentro del objeto: ' + persona.musica);
+
+console.log('Imprimiendo un elemento del arreglo declarado dentro del objeto : ' + persona.musica[2]);
+
+console.log('Agregando elemento \'ranchero\' al arreglo declarado dentro de un objeto ');
+
+persona.musica.push('Ranchero');
+
+console.log('Arreglo con nuevo elemento: ' + persona.musica);
+
+console.log('Ver el objeto declarado dentro del objeto');
+
+console.log(persona.hogar);
+
+console.log('Accediendo al elemento \'ciudad\' del objeto declarado dentro del objeto');
+
+console.log(persona.hogar.ciudad);
+
+console.log('Fecha de nacimiento declarada dentro del objeto y obtenida por medio e una función');
+
+console.log('Edad: ' + persona.nacimiento());
