00332288508b698c846e8d985fe4f04024655faf
CesarVE <thecesarvargas@gmail.com>
2020-03-19 10:59:39 -0600

    Funcionalidad para agregar en el carrito de compra los cursos seleccionados

--- carrito/js/app.js
+++ carrito/js/app.js
@@ -1,6 +1,8 @@
 //Variables
 const carrito = document.getElementById('carrito');
 const cursos = document.getElementById('lista-cursos');
+/**Para insertar curso seleccionado en el carrito de compras */
+const listaCursos = document.querySelector('#lista-carrito tbody');
 
 //Listeners
 cargarEventoListeners();
@@ -24,8 +26,45 @@ function comprarCurso(evento) {
     }
 }
 
-// --->  Leer datos del curso
+/* --->  Leer datos del curso:  antes de crear este método cuando se seleccionaba el carrito
+                                la información del curso seleccionado mandaba la estrucutra html (el card que contiene la información),
+                                con la función la manda en estructura de un objeto*/
 
 function leerDatosCurso(curso) {
-    console.log(curso);
+
+    const infoCurso = {
+        /**atributo: valor curso.querySelector('img').src  <<-- para acceder de una vez al valor del atributo */
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id')
+    }
+    console.log(infoCurso);
+    insertarCarrito(infoCurso);
+}
+
+
+//Muestra el curso seleccionado en el carrito
+function insertarCarrito(curso) {
+    /**
+     * Crear el elemento en le html donde se mostrara el curso seleccionado
+     */
+    const row = document.createElement('tr');
+    /**
+     * Los valores de curso son extraidos en la función leerDatosCurso(curso)
+     */
+    row.innerHTML = `
+        <td>
+            <img src="${curso.imagen}" width=100>
+        </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>
+        <a href="#" class="borrar-curso" data-id="${curso.id}">x</a>
+        </td>
+    `;
+    /**
+     * Se agrega el template creado
+     */
+    listaCursos.appendChild(row);
 }
