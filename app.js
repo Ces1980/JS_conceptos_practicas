609fb4df1c5733474b7ea4c8498ba160905bb3d7
CesarVE <thecesarvargas@gmail.com>
2020-03-25 17:05:09 -0600

    Creando string como objetos

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
-//Método alternativo para crear objetos
+// String
 
-function Cliente(nombre, saldo) {
-    this.nombre = nombre;
-    this.saldo = saldo;
-    this.tipoCliente = function() {
-        let tipo;
-
-        if (this.saldo > 1000) {
-            tipo = 'Gold';
-        } else if (this.saldo > 500) {
-            tipo = 'Platino';
-        } else {
-            tipo = 'Normal'
-        }
-
-        return tipo;
-    }
-}
-
-const persona = new Cliente('Pedro', 20000);
-
-const persona1 = new Cliente('César', 50000);
+const nombre1 = 'César';
+const nombre2 = new String('Vero');
 
 
+//Tipos de datos
 
-console.log(persona);
-console.log(persona.saldo);
-console.log(persona.nombre);
-console.log(persona.tipoCliente());
-console.log(persona1);
-console.log(persona1.saldo);
-console.log(persona1.nombre);
-console.log(persona1.tipoCliente());
+console.log("Tipos de datos de las instancias creadas............");
+console.log('Nombre 1: ' + typeof nombre1);
+console.log('Nombre 2: ' + typeof nombre2);
