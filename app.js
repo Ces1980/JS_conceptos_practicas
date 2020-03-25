bb1a75955a97086a5cfbfc0e44e7c8748694ba4d
CesarVE <thecesarvargas@gmail.com>
2020-03-25 17:15:39 -0600

    Creando funciones como objetos

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // String
-
 const nombre1 = 'César';
 const nombre2 = new String('Vero');
 
 
 ///Creando numeros como objetos
-
 const numero1 = 20;
 const numero2 = new Number(20);
 
 //Creando booleans como objetos
-
 const boleano1 = true;
 const boleano2 = new Boolean(true);
-console.log(typeof boleano1);
-console.log(typeof boleano2);
-console.log(boleano1);
-console.log(boleano2);
+
+//Creando funciones como objetos
+const function1 = function(a, b) {
+    return a + b;
+}
+const function2 = new Function('a', 'b', 'return a + b')
+
+console.log(typeof function1);
+console.log(typeof function2);
+console.log(function1(1, 2));
+console.log(function2(2, 3));
