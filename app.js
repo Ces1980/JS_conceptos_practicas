b6980c143f0ee71d5ecfb36d331e9d3b39380560
CesarVE <thecesarvargas@gmail.com>
2020-03-25 18:38:58 -0600

    Heredar Prototypes a otro Objeto

--- app.js
+++ app.js
@@ -3,43 +3,27 @@
 function Cliente(nombre, saldo) {
     this.nombre = nombre;
     this.saldo = saldo;
-
 }
 
-
-//Creando un prototipo
-Cliente.prototype.tipoCliente = function() {
-
-    let tipo;
-
-    if (this.saldo > 1000) {
-        tipo = 'Premium'
-    } else if (this.saldo > 499) {
-        tipo = 'Gold'
-    } else {
-        tipo = 'Normal'
-    }
-
-    return tipo;
-}
 
 //Creando protoripo que imprime saldo y nombre
 Cliente.prototype.nombreClienteSaldo = function() {
-    return `Nombre: ${this.nombre}, Tu saldo es de ${this.saldo},
-     Tipo de cliente: ${this.tipoCliente()}`;
-}
-
-//Retirar Saldo
-Cliente.prototype.retirSaldo = function(retiro) {
-    return this.saldo -= retiro;
+    return `Nombre: ${this.nombre}, Tu saldo es de ${this.saldo}`;
 }
 
 const cliente1 = new Cliente('César', 5000);
-const cliente2 = new Cliente('Vero', 600);
-const cliente3 = new Cliente('Vane', 100);
 
-cliente2.retirSaldo(300);
+console.log(cliente1);
 
-console.log(cliente1.nombreClienteSaldo());
-console.log(cliente2.nombreClienteSaldo());
-console.log(cliente3.nombreClienteSaldo());
+//Banca para empresas
+
+function Empresa(nombre, saldo, telefono, tipo) {
+    //Heredar de la clase Cliente 
+    Cliente.call(this, nombre, saldo);
+    this.telefono = telefono;
+    this.tipo = tipo;
+}
+//Heredando el prototipo "nombreClienteSaldo " de clase Cliente a la clase Empresa
+Empresa.prototype = Object.create(Cliente.prototype);
+const empresa = new Empresa('Udemy', 500000, 55556589, 'Educación');
+console.log(empresa.nombreClienteSaldo());
