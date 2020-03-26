0fe664872a3d6e914237ab2312571300a63c974e
CesarVE <thecesarvargas@gmail.com>
2020-03-26 13:34:18 -0600

    Agregando funcionalidad para poder seleccionar los datos del formulario y poder enviar

--- cotizaSeguro/js/app.js
+++ cotizaSeguro/js/app.js
@@ -1,7 +1,45 @@
-//Cotizador constructor
-function Seguro() {}
+//Constructor para seguro
+function Seguro(marca, anio, tipo) {
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
+
+//Todo lo que se muestra
+function Interfaz() {}
 
 
+
+// --> EventListener
+//Cotizar seguro es el id con el cual se identifica el cuerpo del formulario
+const formulario = document.getElementById('cotizar-seguro');
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    //Leer la marca seleccionada del select
+    const marca = document.getElementById('marca');
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+    //Leer el año seleccionado del select
+    const anio = document.getElementById('anio');
+    const anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+    //Leer el valor del radio button
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    //Crear instancia de interfaz
+    const interfaz = new Interfaz();
+
+    //Revisamos que los campos no esten vacios
+    if (marcaSeleccionada === '' || anioSeleccionado === '' || tipo === '') {
+        //Interfaz mosrando un error
+        console.log('Te faltan datos');
+    } else {
+        //Instanciar seguro y mostrar interfaz
+        console.log('Todo correcto');
+    }
+})
 
 /**Fecha que permite que el auto asegurado se maximo veiteaños antiguo
  * que en la fecha actual
