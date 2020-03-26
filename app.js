064fc0c8923acc05835f3894ebbff0ca3a08e384
CesarVE <thecesarvargas@gmail.com>
2020-03-25 19:02:22 -0600

    Creando clases y creando métodos estatícos

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-// Función Object Create
-const Cliente = {
-    imprimirSaldo: function() {
-        return `Holan${this.nombre} tu saldo es de ${this.saldo}`
-    },
-    retirarSaldo: function(retiro) {
+class Cliente {
+
+    constructor(nombre, apellido, saldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.saldo = saldo;
+    }
+
+    imprimirSaldo() {
+        return `Hola ${this.nombre} ${this.apellido}, tu saldo es de ${this.saldo}`
+    }
+
+    tipoCliente() {
+        let tipo;
+
+        if (this.saldo > 10000) {
+            tipo = 'Gold';
+        } else if (this.saldo > 5000) {
+            tipo = 'Platino'
+        } else {
+            tipo = 'normal';
+        }
+        return `Eres un cliente de tipo ${tipo}`;
+    }
+
+    retirarSaldo(retiro) {
+
         return this.saldo -= retiro;
+    }
+
+    static bienvenida() {
+        return 'Bienvenida al cajero';
     }
 }
 
-//Crear el objeto y darle valor a atrinutos
+console.log(Cliente.bienvenida());
 
-const lupe = Object.create(Cliente);
-lupe.nombre = 'Lupe';
-lupe.saldo = 500;
-console.log(lupe);
+const maria = new Cliente('María', 'Martínez', 5500);
 
-
-console.log(lupe.imprimirSaldo());
-
-lupe.retirarSaldo(200);
-
-console.log(lupe.imprimirSaldo());
+console.log(maria.imprimirSaldo());
+console.log(maria.tipoCliente());
+console.log(maria.retirarSaldo(200));
