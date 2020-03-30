cec4783ba7040733cbe541b51ea6dd48c2a9af5e
CesarVE <thecesarvargas@gmail.com>
2020-03-29 20:01:13 -0600

    Funcionalidad que muestra error al enviar gasto y cantidad vacios

--- gastoSemanal/js/app.js
+++ gastoSemanal/js/app.js
@@ -1,5 +1,14 @@
 /**Variables */
-const presupuestoUsuario = prompt('¿Cual es tu presupuesto semanal?');
+let presupuestoUsuario;
+do {
+    presupuestoUsuario = prompt('¿Cual es tu presupuesto semanal?');
+    if (isNaN(presupuestoUsuario) || presupuestoUsuario === '') {
+        alert('Ingrese Numeros......')
+    } else {
+        alert('Datos correctos')
+    }
+} while (isNaN(presupuestoUsuario));
+const formulario = document.getElementById('agregar-gasto');
 let cantidadPresupuesto;
 
 
@@ -20,12 +29,33 @@ class Presupuesto {
 // -->>>Clase de interfaz que maneja todo lo relacionado al html
 class Interfaz {
     insertarPresupuesto(cantidad) {
+        //Obtener selectores e insertar cantidad dinamicamente
         const presupuestoSpan = document.querySelector('span#total');
         const restanteSpan = document.querySelector('span#restante');
 
         //Insertar al html
         presupuestoSpan.innerHTML = `${cantidad}`;
         restanteSpan.innerHTML = `${cantidad}`;
+    }
+
+
+    imprimirMensaje(mensaje, tipo) {
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+        if (tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-sucess');
+        }
+        divMensaje.appendChild(document.createTextNode(mensaje));
+        //Insertar en el DOM
+        //Quitar la alerta al ingresar el gasto despues de 3 segundos
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+        setTimeout(function() {
+            document.querySelector('.primario .alert').remove();
+            //Recetaemos el formulario
+            formulario.reset();
+        }, 3000);
     }
 }
 
@@ -40,9 +70,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
         //Instancia de presupuesto aceptada
         cantidadPresupuesto = new Presupuesto(presupuestoUsuario);
+
         //Instanciar la clase de Interfaz
         const ui = new Interfaz();
         //Accedemos al presupuesto
         ui.insertarPresupuesto(cantidadPresupuesto.presupuesto);
+    }
+});
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    //Leer del formulario de gastos
+    const nombreGastos = document.querySelector('#gasto').value;
+    const cantidadGastos = document.querySelector('#cantidad').value;
+
+    //Instanciar la interfaz
+    const ui = new Interfaz();
+    if (nombreGastos === '' || cantidadGastos === '') {
+        //Dos parametros mensaje y tipo
+        ui.imprimirMensaje('Hubo un error', 'error');
+    } else {
+        console.log('El gasto se agrego');
     }
 });
