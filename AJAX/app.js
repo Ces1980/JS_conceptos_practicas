0dc2edb821d8286597ef634224b280fe40f3a362
CesarVE <thecesarvargas@gmail.com>
2020-03-31 11:18:47 -0600

    Primeros pasos con AJAx

--- AJAX/app.js
+++ AJAX/app.js
@@ -8,12 +8,27 @@ function cargarDatos() {
     xhr.open('GET', 'datos.txt', true);
 
     //Una vez que carga la página
-    xhr.onload = function() {
+    xhr.onreadystatechange = function() {
+        console.log(`Estado ${this.readyState}`);
+        if (this.readyState === 4 && this.status === 200) {
+            console.log(this.responseText);
+        }
+    }
+
+    //ready status
+    /**
+     * 0 -> no inicializado
+     * 1-> Conexion establecida 
+     * 2-> Recibido
+     * 3-> procesando
+     * 4-> Respuesta lista
+     */
+    /* xhr.onload = function() {
             //Estado de la conexion --> this se refiere a xhr que es la variable declarada
             if (this.status === 200) {
                 document.getElementById('listado').innerHTML = `<h1>${this.responseText}</h1>`
             }
-        }
-        //Enviar el request
+        } */
+    //Enviar el request
     xhr.send();
 }
