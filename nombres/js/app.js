36a6bc61fe6f21f88beb8fe026255f221afec70a
CesarVE <thecesarvargas@gmail.com>
2020-04-03 13:51:13 -0600

    Enviando la petición AJAX y leyendo /

--- nombres/js/app.js
+++ nombres/js/app.js
@@ -28,13 +28,15 @@ function cargarNombres(evento) {
      * Se obtiene la cantidad de nombres que se quiere obtener
      */
     const cantidad = document.getElementById('numero').value;
-    console.log(cantidad);
 
     /**
      * Se construye la url conforme los datos del formulario
+     * El proyecto se monto en un servidor local ya que la página en su url oficial
+     * se encuentra fuera de servicio: Se uso xampp como servidor y el proyecto
+     * se agrega dentro de la carpeta c:xampp/htdocs
      */
     let url = '';
-    url += 'http://uinames.com/api/?';
+    url += 'http://localhost/uinames/uinames.com/api/?';
     //Si hay origen agregarlo a la url --> el origen se selecciona según las opciones que se muestran en el formulario
     if (origenSeleccionado !== '') {
         url += `region=${origenSeleccionado}&`;
@@ -49,6 +51,39 @@ function cargarNombres(evento) {
         url += `amount=${cantidad}&`;
     }
 
-    console.log(url);
+    /**
+     *  Conectar con ajax
+     */
+    // Iniciar XMLHTTPRequest
+    //--> Creando objeto
+    const xhr = new XMLHttpRequest();
+    //Abrir la conexión
+    /**
+     * Tres parametros: tipo de petición:'GET', url:url(generada de forma dinamica),  true:petición asincrona
+     */
+    xhr.open('GET', url, true);
+    /**
+     * Datos e impresión del template
+     */
+    xhr.onload = function() {
+        if (this.status === 200) {
+            const nombres = JSON.parse(this.responseText);
+            //Generar el HTML
+            let htmlNombres = '<h3>Nombres Generados</h3>';
 
+            htmlNombres += '<ul class="lista">';
+            //Imprimir cada nombre
+            nombres.forEach(nombre => {
+                htmlNombres += `
+                     <li>${nombre.name}</li>
+                     `;
+            });
+            htmlNombres += '</ul>';
+            //Inyectar el código html
+            document.getElementById('resultado').innerHTML = htmlNombres;
+        }
+    }
+
+    //Enviar la conexion
+    xhr.send();
 }
