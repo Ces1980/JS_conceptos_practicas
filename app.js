46f1f390f8bd863bb0ee717eafa34b63ebc9cca8
CesarVE <thecesarvargas@gmail.com>
2020-04-03 14:25:32 -0600

    Ejemplos básicos de CALLBACKS

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
-class Cliente {
+/**
+ * Una función de callback es una función que se pasa a otra función como un argumento,
+ * que luego se invoca dentro de la función externa para completar algún tipo de rutina o acción.
+ * 
+ * Sin embargo, tenga en cuenta que las callbacks a menudo se utilizan para continuar con la ejecución del código 
+ * después de que se haya completado una operación a sincrónica  — estas se denominan devoluciones de llamada asincrónicas. 
+ */
 
-    constructor(nombre, saldo) {
-        this.nombre = nombre;
-        this.saldo = saldo;
-    }
-
-    imprimirSaldo() {
-        return `Hola ${this.nombre}, tu saldo es de ${this.saldo}`
-    }
+//Callback en forEach
 
 
-    static bienvenida() {
-        return 'Bienvenida al cajero';
-    }
+const ciudades = ['CDMX', 'New York', 'Tokio', 'Berlín', 'Madrid', 'Roma'];
+
+console.log(`LLamada a Callback - forEach con función de flecha: `);
+ciudades.forEach(ciudad => {
+    console.log(ciudad);
+});
+//Inline Callback
+ciudades.forEach(function(ciudad) {
+    console.log(ciudad);
+});
+
+console.log("............Con función definida");
+//Con funcion definida
+function callback(ciudad) {
+    console.log(ciudad);
 }
 
-class Empresa extends Cliente {
-
-    constructor(nombre, saldo, telefono, tipo) {
-        /**Toman los valores de la clase padre */
-        super(nombre, saldo);
-        /**Se asignan valores directamente*/
-        this.telefono = telefono;
-        this.tipo = tipo;
-    }
-    static bienvenida() {
-        return 'Bienvenida al cajero para empresas';
-    }
-}
-
-
-const empresa = new Empresa('Cosita', 550000, 5568223366, 'Costura');
-const cesar = new Cliente('César', 5500);
-console.log(Empresa.bienvenida());
-console.log(empresa);
-console.log(empresa.imprimirSaldo());
-console.log(Cliente.bienvenida());
-console.log(cesar);
-console.log(cesar.imprimirSaldo());
+ciudades.forEach(callback);
