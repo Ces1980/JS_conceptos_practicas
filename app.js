bbb72daa91080d1ae103675d3d58977e1304f71a
CesarVE <thecesarvargas@gmail.com>
2020-04-07 13:06:44 -0500

    Conociendo promise --> uso de then

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-/**
- * Un callback (llamada de vuelta) es una función que recibe como argumento otra función y la ejecuta.
+/*
+*Promises: Una promesa representa un valor que puede estar disponible ahora, en el futuro, o nunca.
+*resolve, es el parametro a tomar cuando se cumple la promesa
+--> resolve necesita de then() para que ejecutar su acción 
+*reject es cuando no se cumple la promesa
  */
 
-
-//Listado de paises
-
-const paises = ['México', 'Canada', 'EEUU', 'España', 'Brazil', 'India'];
-
-
-// Callback         los paises se muestran despues de 3 segundos
-function nuevoPais(pais, mostrarPaises) {
+const esperando = new Promise(function(resolve, reject) {
     setTimeout(function() {
-        paises.push(pais);
-        //Se agrega un nuevo país y después se vuelve a ejecutar la funcion de mostrar pais
-        mostrarPaises();
-    }, 3000);
-}
+        resolve('Se ejecuto');
+    }, 5000);
+});
 
-//Los paises se muestran después de 2 segundos
-function mostrarPaises() {
-    setTimeout(function() {
-        let html = '';
-        paises.forEach(function(pais) {
-            html += `<li>${pais}</li>`;
-        });
-        document.getElementById('app').innerHTML = html;
-    }, 2000);
-}
-
-
-//Agregar nuevo pais
-nuevoPais('Guatemala', mostrarPaises);
-
-//Mandar llamar a la función
-mostrarPaises();
+esperando.then(function(mensaje) {
+    console.log(mensaje);
+});
