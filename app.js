cd71062c19b84ea0b9d528ff42d5c5976f019180
CesarVE <thecesarvargas@gmail.com>
2020-04-07 13:16:38 -0500

    Conociendo promise --> uso de catch

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 *Promises: Una promesa representa un valor que puede estar disponible ahora, en el futuro, o nunca.
 *resolve, es el parametro a tomar cuando se cumple la promesa
 --> resolve necesita de then() para que ejecutar su acción 
-*reject es cuando no se cumple la promesa
+* El método then es el retorno de la promesa, puede ser cumplida o rechazada 
+* Así como el resolve necesita de then() para mostra su mensaje, reject necesita de catch
+* para mostra el suyo resolve va de la mano con then() y reject va de la mano con catch()  
  */
 
-const esperando = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-        resolve('Se ejecuto');
-    }, 5000);
+const aplicarDescuento = new Promise(function(resolve, reject) {
+    const descuento = false;
+    if (descuento) {
+        resolve('Descuento aplicado');
+    } else {
+        reject('No se puede aplicar el descuento');
+    }
 });
 
-esperando.then(function(mensaje) {
+aplicarDescuento.then(function(mensaje) {
     console.log(mensaje);
+}).catch(function(error) {
+    console.log(error);
 });
