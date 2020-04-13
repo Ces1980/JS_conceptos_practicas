b0e870bf57825269518088ab5d39737f4bbdbd0c
CesarVE <thecesarvargas@gmail.com>
2020-04-13 17:46:56 -0500

    Asignación por Destructuring: asignando valores por default

--- app.js
+++ app.js
@@ -4,28 +4,12 @@
   tanto de arreglos como de propiedades de objetos, en variables distintas. 
  */
 const cliente = {
-    tipo: 'Premium',
-    nombre: 'Vanessa',
-    datos: {
-        ubicacion:{
-            ciudad: 'Ecatepec',
-            pais: 'México'
-        },
-        cuenta:{
-            desde: '10-12-1012',
-            saldo: 5000
-        }
-    }
+    nombre:'Aurelio',
+
 }
 
-let {datos: {ubicacion}} = cliente;
-let {datos: {cuenta}} = cliente;
+let {nombre, tipo='Básico', saldo= 0} = cliente;
 
-console.log(cliente)
-console.log(ubicacion)
-console.log(ubicacion.ciudad)
-console.log(ubicacion.pais)
-
-console.log(cuenta)
-console.log(cuenta.desde)
-console.log(cuenta.saldo)
+console.log(nombre)
+console.log(tipo)
+console.log(saldo)
