b412357b69d08a330d4295b7bd9b89b9800e89a9
CesarVE <thecesarvargas@gmail.com>
2020-04-13 13:27:45 -0500

    EVENTOS: Leyendo los datos en el formulario

--- eventos/js/app.js
+++ eventos/js/app.js
@@ -1,3 +1,23 @@
 //Instanciar las clases
 const eventBrite = new EventBrite();
 const ui = new Interfaz();
+
+//listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e)=> {
+    e.preventDefault();
+//Comprobar si la funcion del listener funciona console.log('Presionado')
+
+    //Leer el texto del input
+    const textoBuscador = document.getElementById('evento').value;
+    //Leer el select
+    const categorias = document.getElementById('listado-categorias');
+    const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+    /* console.log(textoBuscador)
+    console.log(categoriaSeleccionada) */
+    //Revisar que haya algo escrito en el buscador
+    if(textoBuscador !== ''){
+        console.log('Buscando')
+    }else{
+        console.log('No hay nada')
+    }
+})
