7458cea84c5919dcb1f011f873d317621a16f850
CesarVE <thecesarvargas@gmail.com>
2020-04-15 22:58:29 -0500

    DESTRUCTURING a Arreglos y objetos: extracción de datos en objetos y arreglos

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-//
-const ciudades = ['Londres','Nuw York', 'CDMX', 'Madrid'];
+/**
+ * La sintaxis de asignación desestructurante (destructuring assignment) 
+ * es una expresión que posibilita la extracción de datos, 
+ * tanto de arreglos como de propiedades de objetos, 
+ * en variables distintas.
+ */
+//Declaración del objeto
+const cliente = {
+    tipo: 'Premium', 
+    saldo: 30000,
+    datos: {
+        nombre: 'César',
+        apellido: 'Vargas',
+        residencia: {
+            ciudad: 'México'
+        }
+    },
+    movimientos: ['12-03-2018', '25-05-2018', '05-08-2020']
+}
 
-/* const [
-    primeraCiudad, 
-    segundaCiudad,
-    terceraCiudad,
-    cuartaCiudad
-] = ciudades; */
+//Variable que permite mandar llamar los datos
+let {
+    tipo,
+    datos,
+    movimientos,
+    movimientos: [uno,dos,tres],
+    datos: {residencia}
+} = cliente;
 
-/***Accediendo a un valor especifico según la posición en el arreglo */
-const [,,,ciudad]= ciudades;
-
-console.log(ciudad)
+console.log(tres)
+console.log(cliente)
+console.log(datos.apellido)
+console.log(tipo)
+console.log(residencia)
+console.log(datos)
