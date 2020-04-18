ccd553061efe4db414b38b0aa4c427c5ee7756a8
CesarVE <thecesarvargas@gmail.com>
2020-04-18 09:07:57 -0500

    FUNCIONES REGULARES: formas de declararlas

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-//GENERADORES
+//EXPRESIONES REGULARES
 
-function *nuevoGenerador(carrito) {
+/* Formas de declarar una expresion regular */
+const exp1 = new RegExp('/abc/');
+const exp2 = /abc/;
 
-        for (let i = 0; i < carrito.length; i++) {
-            yield carrito[i];
-            
-        }
-}
-
-// Creamos el carrito
-const carrito = ['Producto1','Producto2','Producto3','Producto4','Producto5'];
-
-// Recorrimos el carrito
-let iterador = nuevoGenerador(carrito);
-
-//Imprimimos
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
+console.log(exp1);
+console.log(exp2);
