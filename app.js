6fc7749bd23a1255be29bfaba35e9d7051d7c7b0
CesarVE <thecesarvargas@gmail.com>
2020-04-18 08:16:30 -0500

    MAPS: métodos de máp-> has, get, set, clear, delete

--- app.js
+++ app.js
@@ -7,9 +7,29 @@ cliente.set('nombre', 'Vero');
 cliente.set('tipo', 'premium');
 cliente.set('saldo', 3000);
 /**Accediendo a los valores de un MAP */
-console.log(cliente.get('nombre'));
+/* console.log(cliente.get('nombre'));
 console.log(cliente.get('tipo'));
-console.log(cliente.get('saldo'));
+console.log(cliente.get('saldo')); */
+
+/* Métodos para los maps */
+console.log(cliente)
+//Tamaño del map
+console.log(cliente.size)
+//Comporbando valor dentro del MAP
+console.log(cliente.has('apellido'));
+//devolver el valor de MAp
+console.log(cliente.get('tipo'));
+//Borrar una propiedad del MAP
+console.log(cliente.delete('nombre'));
 
 console.log(cliente)
 
+//Agregando propiedad al map
+cliente.set('nombre', 'Gloria');
+
+console.log(cliente)
+
+/* Limpiando el MAP */
+
+cliente.clear();
+console.log(cliente)
