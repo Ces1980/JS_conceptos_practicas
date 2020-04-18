d0f5bc3f69336eed2476ef7c8ac57997b7ae33d8
CesarVE <thecesarvargas@gmail.com>
2020-04-18 08:08:56 -0500

    MAPS: inicializando, agregando y accediendo a los valores

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-//SET
-/* Los objetos Set son colecciones de valores. 
-Se puede iterar sus elementos en el orden de su inserción.
-Un valor en un Set sólo puede estar una vez; éste es único en la colección Set. */
+//MAPS
 
+/* Inicializando un MAP */
+let cliente = new Map();
+/**Agredando valor a un map */
+cliente.set('nombre', 'Vero');
+cliente.set('tipo', 'premium');
+cliente.set('saldo', 3000);
+/**Accediendo a los valores de un MAP */
+console.log(cliente.get('nombre'));
+console.log(cliente.get('tipo'));
+console.log(cliente.get('saldo'));
 
-/* Inicializando un SET */
-let carrito = new Set();
-//Agregando elementos a la collección
-carrito.add('Camisa');
-carrito.add('Disco #1');
-carrito.add('Disco #2');
-carrito.add('Disco #3');
-//Un disco cuando observa un valor duplicado no lo añade a la colección
-carrito.add('Disco #3');
+console.log(cliente)
 
-//Comprobando que existe un valor en un SET console.log(carrito.has('Guitarra'));
-//Recorrer set con foreach
-console.log('Pinta todos los elementos de la coleccion SET')
-carrito.forEach((producto, index) => {
-    console.log(`${index}: ${producto}`)
-});
-/* En un SET la llave y el valor son iguales */
-
-/* Convertir un SET a Arreglo */
-const arregloCarrito = [...carrito];
-console.log(arregloCarrito);
-/**Al convertir un SET en un arreglo se le pueden aplicar todas las caracteristicas
- * se pueden utilizar en arreglo
- */
