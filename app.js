46122872e0f8b6d7df88bb7bb7ce074c3f7a9489
CesarVE <thecesarvargas@gmail.com>
2020-04-18 09:15:16 -0500

    FUNCIONES REGULARES: caracteristicas de la función regular

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 //EXPRESIONES REGULARES
+/*  Las expresiones regulares son patrones utilizados para encontrar 
+*  una determinada combinación de caracteres dentro de una cadena de texto. 
+*  En JavaScript, las expresiones regulares también son objetos. 
+*  Estos patrones se utilizan en los métodos exec y test de RegExp, 
+*  así como los métodos match, replace, search y split de String.  */
 
-/* Formas de declarar una expresion regular */
-const exp1 = new RegExp('/abc/');
-const exp2 = /abc/;
+let valor, expReg;
 
-console.log(exp1);
-console.log(exp2);
+expReg = /[123456789]/;
+/* expReg con rango */
+expReg =/[1-9]/;
+
+valor = 1992;
+/**Verificar si los numeros de valor se encuentran dentro de la expresion regular */
+console.log(expReg.test(valor));
+
