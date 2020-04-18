be9d397ad0ef218f4f020fe17f16feba5e9a006e
CesarVE <thecesarvargas@gmail.com>
2020-04-18 07:40:07 -0500

    SYMBOL: accediendo a la propiedad SYMBOL y conociendo su caracteristica de propiedad

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 //SYMBOL
 
 
-const simbolo = Symbol();
-const simbolo2 = Symbol('Descrpción aquí');
+let nombre = Symbol();
+let apellido  = Symbol();
 
-console.log( typeof simbolo)
-console.log(simbolo2)
+//Crear una persona
+let persona = {}
+//Se agrega propiedad nueva al objeto
+persona.nombre='César';
+//Agegrar a persona atributo symbol
+persona[nombre] = 'César';
+persona[apellido]= 'Vargas';
+//Agregando más propiedades al objeto persona
+persona.saldo=100;
+persona.tipoCliente= 'Normal';
+
+//Accediendo a las propiedades del objeto
+console.log(persona.nombre)
+//Accediendo al Symbol del objeto
+console.log(persona[nombre])
+
+//Los Symbols tienen propiedades privadas
+//Con el siguiente for se muestran las propiedades del objeto pero no 
+//las propiedades declaradas como SYMBOL
+for (let i in persona) {
+    console.log(`${i} : ${persona[i]}`);
+}
+
+console.log(persona)
