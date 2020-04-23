a10606ad6b7d573f11a64f3048e60975cd866382
CesarVE <thecesarvargas@gmail.com>
2020-04-23 02:21:30 -0500

    BUSCADOR DE CANCIONES: Creando el módulo de interfaz

---
+++ letrasjs/js/interfaz.js
@@ -0,0 +1,5 @@
+// Seleccionamos en formulario los campos donde vamos a generar interacción
+export const formularioBuscar = document.querySelector('#formulario-buscar'),
+                    divBuscar = document.querySelector('#buscar'),
+                  divMensajes = document.querySelector('#mensajes'),
+                 divResultado = document.querySelector('#resultado');
