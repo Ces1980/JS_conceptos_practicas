b27235a7a5b539c0bd6891a071d2386ee880b424
CesarVE <thecesarvargas@gmail.com>
2020-04-25 11:02:22 -0500

    IIFE: funciones autoejecutables

--- PatronesJS/js/module.js
+++ PatronesJS/js/module.js
@@ -1,17 +1,34 @@
-const comparBoleto = ( function(){
+/* Funciones autoejecutables
+ *Las expresiones de función ejecutadas inmediatamente (IIFE por su sigla en inglés)
+ son funciones que se ejecutan tan pronto como se definen.
 
+(function () {
+    statements
+})();
 
-    // Privado --> no puede ser accedido el valor de la variable desde fuera del método
+Es un patrón de diseño también conocido cómo función autoejecutable (Self-Executing Anonymous Function  )
+y se compone por dos partes. La primera es la función anónima con alcance léxico encerrado por el
+Operador de Agrupación (). Esto impide accesar variables fuera del IIFE, así
+cómo contaminar el alcance (scope) global.
+
+La segunda parte crea la expresión de función cuya ejecución es inmediata (), siendo
+interpretado directamente en el engine de JavaScript.
+*/
+const comprarBoleto = ( function(){
+
+    //Privado
     let evento = 'Conferencia JS 2020';
+    let precio = 200;
 
-
-    //Publico --> solo puede ser accedido con un return
     return{
-        mostarBoleto: function(){
-            console.log(evento)
+        // evento
+        mostrarBoleto: function(){
+            console.log(evento);
+            console.log(precio);
         }
     }
 })();
 
 
-comparBoleto.mostarBoleto()
+// console.log(evento)
+comprarBoleto.mostrarBoleto()
