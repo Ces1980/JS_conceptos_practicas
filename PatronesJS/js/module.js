859c4f5ee18df2e107d21a5fadc6c2bcf9b93294
CesarVE <thecesarvargas@gmail.com>
2020-04-25 09:43:29 -0500

    PATRONESDISEÑO: module-> retornando el valor de la variable

---
+++ PatronesJS/js/module.js
@@ -0,0 +1,15 @@
+const comparBoleto = ( function(){
+
+
+    // Privado --> no puede ser accedido el valor de la variable desde fuera del método
+    let evento = 'Conferencia JS 2020';
+
+
+    //Publico --> solo puede ser accedido con un return
+    return{
+        evento
+    }
+})();
+
+
+console.log(comparBoleto.evento)
