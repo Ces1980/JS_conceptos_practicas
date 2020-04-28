68fa169320d9a8e69e91bcd747d9eed17cadfa8b
CesarVE <thecesarvargas@gmail.com>
2020-04-28 17:09:04 -0500

    BUSCADOR DE AUTOS: mostrando en la vista los datos del arreglo

--- HigherOrderFunctions_ESP/js/buscador.js
+++ HigherOrderFunctions_ESP/js/buscador.js
@@ -1,11 +1,168 @@
 // crear los años
 const years = document.createElement('option');
-const  max = new Date().getFullYear();
-let  min = max - 10;
+const max = new Date().getFullYear();
+let min = max - 10;
 
-for(let i = max; i >  min; i--) {
-    let option =  document.createElement('option');
+for (let i = max; i > min; i--) {
+    let option = document.createElement('option');
     option.value = i;
     option.innerText = i;
     document.querySelector('#year').appendChild(option);
+}
+
+/* Función que permite llamar la lista de autos */
+function obtenerAutos() {
+
+    return [
+        {
+            marca: 'BMW',
+            modelo: 'Serie 3',
+            year: 2012,
+            precio: 30000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A4', year: 2018, precio: 40000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+        {
+            marca: 'Ford',
+            modelo: 'Mustang',
+            year: 2015,
+            precio: 20000,
+            puertas: 2,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A6', year: 2010, precio: 35000, puertas: 4, color: 'Negro', transmision: 'automatico' },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 5',
+            year: 2016,
+            precio: 70000,
+            puertas: 4,
+            color: 'Rojo',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'Mercedes Benz',
+            modelo: 'Clase C',
+            year: 2015,
+            precio: 25000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'Chevrolet',
+            modelo: 'Camaro',
+            year: 2018,
+            precio: 60000,
+            puertas: 2,
+            color: 'Rojo',
+            transmision: 'manual'
+        },
+        { marca: 'Ford', modelo: 'Mustang', year: 2019, precio: 80000, puertas: 2, color: 'Rojo', transmision: 'manual' },
+        {
+            marca: 'Dodge',
+            modelo: 'Challenger',
+            year: 2017,
+            precio: 40000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A3', year: 2017, precio: 55000, puertas: 2, color: 'Negro', transmision: 'manual' },
+        {
+            marca: 'Dodge',
+            modelo: 'Challenger',
+            year: 2012,
+            precio: 25000,
+            puertas: 2,
+            color: 'Rojo',
+            transmision: 'manual'
+        },
+        {
+            marca: 'Mercedes Benz',
+            modelo: 'Clase C',
+            year: 2018,
+            precio: 45000,
+            puertas: 4,
+            color: 'Azul',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 5',
+            year: 2019,
+            precio: 90000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Ford', modelo: 'Mustang', year: 2017, precio: 60000, puertas: 2, color: 'Negro', transmision: 'manual' },
+        {
+            marca: 'Dodge',
+            modelo: 'Challenger',
+            year: 2015,
+            precio: 35000,
+            puertas: 2,
+            color: 'Azul',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 3',
+            year: 2018,
+            precio: 50000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'BMW',
+            modelo: 'Serie 5',
+            year: 2017,
+            precio: 80000,
+            puertas: 4,
+            color: 'Negro',
+            transmision: 'automatico'
+        },
+        {
+            marca: 'Mercedes Benz',
+            modelo: 'Clase C',
+            year: 2018,
+            precio: 40000,
+            puertas: 4,
+            color: 'Blanco',
+            transmision: 'automatico'
+        },
+        { marca: 'Audi', modelo: 'A4', year: 2016, precio: 30000, puertas: 4, color: 'Azul', transmision: 'automatico' }
+    ];
+
+
+}
+
+// se deposita en una variable el contenido de la función obtenerAutos
+const autos = obtenerAutos();
+
+//Event Listener
+/* función que permite cargar el docuemnto */
+document.addEventListener('DOMContentLoaded', () => {
+    // Cuando se cargue la función mostrarAtos se pasa como parametro todos los automoviles
+    mostrarAutos(autos);
+});
+
+function mostrarAutos(autos) {
+
+    // Leer el elemento resultado
+    const contenedor = document.querySelector('#resultado');
+
+    autos.forEach(auto => {
+        const autoHTML = document.createElement('p');
+        autoHTML.innerHTML = `
+            <p>${auto.marca} ${auto.modelo} - ${auto.year} - ${auto.puertas} Puertas -
+            Trasmisión: ${auto.transmision} - Precio: ${auto.precio} - Color: ${auto.color} </p>
+        `;
+        contenedor.appendChild(autoHTML);
+    });
 }
