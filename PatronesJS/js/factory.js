652ef592c0d6baa6371b06f888ce78370181ea9a
CesarVE <thecesarvargas@gmail.com>
2020-04-27 21:04:42 -0500

    PATRONESDISEÑO: builder-> creación de objetos complejos desde un objeto fuente

--- PatronesJS/js/factory.js
+++ PatronesJS/js/factory.js
@@ -2,7 +2,7 @@
 Esto es especialmente útil cuando no sabemos, en tiempo de diseño, el subtipo que vamos a utilizar
 o cuando queremos delegar la lógica de creación de los objetos a una clase Factory.
  Utilizando este patrón podemos crear instancias dinámicamente mediante la configuración, estableciendo cual será la implementación a utilizar en un archivo de texto, XML, properties o mediante cualquier otra estrategia. */
-
+// @ts-nocheck
 function constructorSitios() {
 
     this.crearElemento = function (texto, tipo) {
