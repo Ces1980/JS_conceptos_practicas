b172fafee577c458478176391aa7c614d9ce5362
CesarVE <thecesarvargas@gmail.com>
2020-04-29 23:25:40 -0500

    INDEXEDDB_Citas veterinaria: Creando la base de datos

--- indexedDB/js/app.js
+++ indexedDB/js/app.js
@@ -11,3 +11,22 @@ const form = document.querySelector('#formulario'),
     sintomas = document.querySelector('#sintomas'),
     citas = document.querySelector('#citas'),
     headingAdministra = document.querySelector('#administra');
+
+//Esperar por el DOM Ready
+document.addEventListener('DOMContentLoaded', () => {
+    //Crear la base de datos
+    let crearDB = window.indexedDB.open('citas', 1);
+
+    //Si hay un error enviarlo a la consola
+    crearDB.onerror = function () {
+        console.err('Hubo un error')
+    }
+    // Si todo esta bien entonces mostrar en consola y asignar la base de datos
+    crearDB.onsuccess = function () {
+        console.log('Todo listo ....!')
+
+        //Asignar a la base de datos
+        DB = crearDB.result;
+        console.log(DB);
+    }
+});
